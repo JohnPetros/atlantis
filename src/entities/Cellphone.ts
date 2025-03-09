9f911ac8299de3020618e97de61211b489052101
JohnPetros <joaopedro.nc@outlook.com>
2025-03-09 10:16:42 -0300

    ✨ feat: implement cellphone crud menu

--- src/entities/Cellphone.ts
+++ src/entities/Cellphone.ts
@@ -11,8 +11,20 @@ export class Cellphone extends Entity<CellphoneProps> implements Prototype {
     return this.props.ddd
   }
 
+  set ddd(ddd: string) {
+    this.props.ddd = ddd
+  }
+
   get number(): string {
     return this.props.number
+  }
+
+  set number(number: string) {
+    this.props.number = number
+  }
+
+  get formattedValue(): string {
+    return `(${this.props.ddd}) ${this.props.number}`
   }
 
   public clone(): Cellphone {
