cbc8731954ab7801b44be8dfc7967b869fca1e64
JohnPetros <joaopedro.nc@outlook.com>
2025-03-09 10:28:41 -0300

    ✨ feat: implement HolderEditingMenu

---
+++ src/menus/HolderEditingMenu.ts
@@ -0,0 +1,17 @@
+import type { Input } from '@/interfaces/Input'
+import type { Menu } from '@/interfaces/Menu'
+
+export class HolderEditingMenu implements Menu {
+  constructor(private input: Input) {}
+
+  async display(): Promise<string> {
+    return await this.input.select('Selecione um valor para editar', [
+      ['Nome', 'name'],
+      ['Nome social', 'socialName'],
+      ['Data de nascimento', 'birthDate'],
+      ['Documentos', 'documents'],
+      ['Endereço', 'address'],
+      ['Telefones', 'cellphones'],
+    ])
+  }
+}
